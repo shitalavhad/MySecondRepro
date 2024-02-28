2c6f5030aec7c226be85b82fafca4fc02233f56e
SAI <SAI@DESKTOP-AI6NF5T>
2024-02-27 22:40:56 -0800

    First Commit

---
+++ DemoJenkins/src/main/java/demoJenkins/Demo.java
@@ -0,0 +1,30 @@
+package demoJenkins;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void lunchBrowser()
+	{
+System.setProperty("webdriver.chrome.driver","G:\\chromedriver_win32 (6)\\chromedriver-win64\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.facebook.com");
+		
+		System.out.println("Page Title:"+driver.getTitle());
+		
+		System.out.println("Page Title:"+driver.getCurrentUrl());
+		
+		driver.close();
+		
+	}
+}
