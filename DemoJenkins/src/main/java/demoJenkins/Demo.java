5360000e85f17bc43eb8461e9b3b2cc31382b056
SAI <SAI@DESKTOP-AI6NF5T>
2024-02-28 21:16:26 -0800

    Second cheange in class

--- DemoJenkins/src/main/java/demoJenkins/Demo.java
+++ DemoJenkins/src/main/java/demoJenkins/Demo.java
@@ -22,7 +22,9 @@ System.setProperty("webdriver.chrome.driver","G:\\chromedriver_win32 (6)\\chrome
 		
 		System.out.println("Page Title:"+driver.getTitle());
 		
-		System.out.println("Page Title:"+driver.getCurrentUrl());
+		System.out.println("Page currentUrl:"+driver.getCurrentUrl());
+		
+		System.out.println("Page souece code:"+driver.getPageSource());
 		
 		driver.close();
 		
