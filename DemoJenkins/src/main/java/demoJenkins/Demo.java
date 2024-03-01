934e4fdeedf87649c0aeabcb12ef697014150dd0
shital_avhad <120552343+shitalavhad@users.noreply.github.com>
2024-03-01 03:34:43 -0800

    Update Demo.java

--- DemoJenkins/src/main/java/demoJenkins/Demo.java
+++ DemoJenkins/src/main/java/demoJenkins/Demo.java
@@ -23,8 +23,8 @@ System.setProperty("webdriver.chrome.driver","G:\\chromedriver_win32 (6)\\chrome
 		System.out.println("Page Title:"+driver.getTitle());
 		
 		System.out.println("Page currentUrl:"+driver.getCurrentUrl());
-		System.out.println("Page currentUrl:"+driver.getCurrentUrl());
-		System.out.println("Page souece code:"+driver.getPageSource());
+		
+		
 		System.out.println("Page souece code:"+driver.getPageSource());
 		
 		driver.close();
