d21e5a922a853b5a350b2adf8a0ef3540c5b5dba
shital_avhad <120552343+shitalavhad@users.noreply.github.com>
2024-03-01 02:58:53 -0800

    Update Demo.java

--- DemoJenkins/src/main/java/demoJenkins/Demo.java
+++ DemoJenkins/src/main/java/demoJenkins/Demo.java
@@ -21,9 +21,13 @@ System.setProperty("webdriver.chrome.driver","G:\\chromedriver_win32 (6)\\chrome
 		driver.get("https://www.facebook.com");
 		
 		System.out.println("Page Title:"+driver.getTitle());
+
+
+
+
 		
 		System.out.println("Page currentUrl:"+driver.getCurrentUrl());
-		
+		System.out.println("Page currentUrl:"+driver.getCurrentUrl());
 		System.out.println("Page souece code:"+driver.getPageSource());
 		System.out.println("Page souece code:"+driver.getPageSource());
 		
